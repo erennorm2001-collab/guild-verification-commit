7d467f8ec7f24416698bd11bce02338db7d20d3a
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:04:07 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:04:01.439Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T19:04:09.098Z
+Commit: Add multi-language support
