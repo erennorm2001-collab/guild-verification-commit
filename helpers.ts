fa4f2933730dd7a796aacf8838d0f1fa288207d7
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:03:48 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:03:16.651Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T19:03:49.854Z
+Commit: Fix broken links in documentation
