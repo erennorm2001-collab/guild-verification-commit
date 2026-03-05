114542e024b6173ad331466640dad3cae18170c5
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:00:30 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:00:11.013Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T19:00:31.719Z
+Commit: Fix memory leak in event handlers
