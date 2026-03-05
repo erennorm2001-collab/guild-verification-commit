2e519f0eefe746d74a7a49d1911c932e3aa950ec
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 21:58:49 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:58:43.334Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T18:58:51.349Z
+Commit: Fix broken links in documentation
