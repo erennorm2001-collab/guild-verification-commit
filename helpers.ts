79bd07bbb248af478f20d2f026275e21b7c4ec79
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:00:12 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:00:04.858Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T19:00:13.805Z
+Commit: Fix broken links in documentation
