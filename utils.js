4c23b04e3ca72d80e12143adcf88bac89bce6365
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 21:58:52 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:58:50.109Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:58:53.812Z
+Commit: Resolve null pointer exception in user service
