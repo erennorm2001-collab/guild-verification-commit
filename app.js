927cac9f865b03b35e57b84475cce84e935a7f36
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 21:59:33 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:58:17.438Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:59:35.091Z
+Commit: Fix broken links in documentation
