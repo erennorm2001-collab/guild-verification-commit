6e2b2236ff98f5326199ceb60b15325253bbd85f
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:02:51 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:01:05.414Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T19:02:53.140Z
+Commit: Fix broken links in documentation
