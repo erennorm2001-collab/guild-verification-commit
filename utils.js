0f263321787a73c961d86b84a8401db0bd55c50c
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:00:47 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:00:31.719Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T19:00:49.116Z
+Commit: Fix CSS styling bug on mobile devices
