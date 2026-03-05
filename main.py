f5b058dad4b639b88b979d0e6a63b7f5687c6f41
erennorm2001-collab <erennorm2001@gmail.com>
2026-03-05 22:05:46 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:05:13.964Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T19:05:47.633Z
+Commit: Resolve null pointer exception in user service
